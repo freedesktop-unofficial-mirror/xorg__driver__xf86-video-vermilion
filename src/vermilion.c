083e15c44cc722aae7b6d02fc4856a3f2d0f5153
Peter Hutterer <peter.hutterer@who-t.net>
2009-07-16 11:58:18 +1000

    Update to xextproto 7.1 support.
    DPMS header was split into dpms.h (client) and dpmsconst.h (server). Drivers
    need to include dpmsconst.h if xextproto 7.1 is available.
    
    Signed-off-by: Peter Hutterer <peter.hutterer@who-t.net>

--- src/vermilion.c
+++ src/vermilion.c
@@ -59,8 +59,13 @@
 #include "xf86cmap.h"
 
 /* DPMS */
+#ifdef HAVE_XEXTPROTO_71
+#include <X11/extensions/dpmsconst.h>
+#else
 #define DPMS_SERVER
 #include <X11/extensions/dpms.h>
+#endif
+
 #include "xf86Priv.h"
 
 #define KERNELNAME "Vermilion Range"
